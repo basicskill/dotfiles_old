4aa0a5a87bd841f2ecf6c2f4225e212c4fb077e8
basicskill <mladenbasic99@gmail.com>
2018-04-13 22:35:42 +0200

    .

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -36,7 +36,7 @@ c.aliases = {
 ## Valid values:
 ##   - webengine: Use QtWebEngine (based on Chromium).
 ##   - webkit: Use QtWebKit (based on WebKit, similar to Safari).
-# c.backend = 'webengine'
+c.backend = 'webengine'
 
 ## This setting can be used to map keys to other keys. When the key used
 ## as dictionary-key is pressed, the binding for the key used as
