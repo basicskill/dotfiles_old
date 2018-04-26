7b59c9fb90834cfdf454b81656e3679c232fe79c
basicskill <mladenbasic99@gmail.com>
2018-04-26 22:17:20 +0200

    resenj compton

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 ## Promenljive
 
 dir=~/dotfiles
-files="config/i3 config/qutebrowser config/startpage config/ranger Xdefaults Xmodmap vimrc bashrc scripts"
+files="config/i3 config/qutebrowser config/startpage config/ranger config/compton.conf Xdefaults Xmodmap vimrc bashrc scripts"
 
 ## Linkovanje
 for file in $files; do
